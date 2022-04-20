124f99516df1a950f1802c93889495636c71adc5
autumnc89 <96134543+autumnc89@users.noreply.github.com>
2022-04-20 11:36:53 -0500

    rename index

--- Resources/JavaScript/taskmanager.js
+++ Resources/JavaScript/taskmanager.js
@@ -11,19 +11,8 @@ class taskManager{
 }
 
 let mate = new taskManager("Mike Will", "Betty", "Task 1", "add Javascript");
-mate.manageTask();  // result: Mike Will is 15 years old!
+mate.manageTask();  // result: 
 console.log(mate.manageTask())
 
 
-// class ClassMates{
-// 	constructor(name,age){
-//     	this.name=name;
-//       	this.age=age;
-//     }
-//   	displayInfo(){
-//     	return this.name + "is " + this.age + " years old!";
-//     }
-// }
 
-// let classmate = new ClassMates("Mike Will",15);
-// classmate.displayInfo();  // result: Mike Will is 15 years old!
